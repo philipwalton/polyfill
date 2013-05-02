a022d28e87d48c73bf75f0902af8480dbdaf9c68
Philip Walton <philip@philipwalton.com>
2013-05-01 22:38:06 -0700

    Add initial demo of position: sticky;

---
+++ demos/position-sticky/position-sticky.js
@@ -0,0 +1,37 @@
+;(function($) {
+
+  Polyfill({declarations:["position:*stickier"]}).then(function(matched, unmatched) {
+
+    matched.each(function(rule) {
+
+      var $el = $(rule.getSelectors())
+        , offset = $el.offset().top
+        , top = rule.getDeclaration().top
+        , $clone
+
+      function onScroll(e) {
+        if ($(window).scrollTop() >= offset) {
+          if (!$clone) {
+            $clone = $el.clone().css({position: "fixed", top: top}).appendTo("body")
+            $el.css("visiblity", "hidden")
+          }
+        } else {
+          $clone && $clone.remove()
+          $clone = null
+          $el.removeAttr("style")
+        }
+      }
+
+      onScroll()
+      $(document).on("scroll", onScroll);
+
+
+    })
+
+
+
+
+
+  })
+
+}(jQuery))
